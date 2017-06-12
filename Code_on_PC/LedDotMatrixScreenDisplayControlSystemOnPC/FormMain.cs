c019c2edfc92e0fb13c8aab81cc7a6d7bba1bd52
MrHeer <hlm52pk@163.com>
2017-06-12 16:54:16 +0800

    create the proj
    LedDotMatrixScreenDisplayControlSystem(51,PC)

---
+++ Code_on_PC/LedDotMatrixScreenDisplayControlSystemOnPC/FormMain.cs
@@ -0,0 +1,18 @@
+﻿using System.Resources;
+using System.Windows.Forms;
+
+namespace LedDotMatrixScreenDisplayControlSystemOnPC
+{
+    public partial class FormMain : Form
+    {
+        private ResourceManager res = new ResourceManager(typeof(FormMain));
+
+        public FormMain()
+        {
+            InitializeComponent();
+            this.Text = Res.GetString("StringAppName");
+        }
+
+        private ResourceManager Res { get => res; }
+    }
+}
